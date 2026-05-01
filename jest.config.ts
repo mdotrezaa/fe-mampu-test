89cf75a4dc9cad2d91ca139847fe0cd440522729
Muhammad Reza <rezamuhammad23@gmail.com>
2026-05-01 11:00:46 +0700

    setup

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+// jest.config.ts
+import type { Config } from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
+
+export default config;
